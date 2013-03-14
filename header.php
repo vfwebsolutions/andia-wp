395327b86e16471a92f93db7b6a9b1e330085891
Vitor Faiante <vfwebsolutions@gmail.com>
2013-03-14 17:07:44 +0000

    Updated <title> tag to be more SEO Friendly
    Signed-off-by: Vitor Faiante <vfwebsolutions@gmail.com>

--- header.php
+++ header.php
@@ -11,7 +11,26 @@
     <head>
 
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <title>Andia - Responsive Agency Template</title>
+        <title>
+           <?php 
+              if (function_exists('is_tag') && is_tag()) { 
+                 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; } 
+              elseif (is_archive()) { 
+                 wp_title(''); echo ' Archive - '; } 
+              elseif (is_search()) { 
+                 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } 
+              elseif (!(is_404()) && (is_single()) || (is_page())) { 
+                 wp_title(''); echo ' - '; } 
+              elseif (is_404()) { 
+                 echo 'Not Found - '; } 
+              if (is_home()) { 
+                 bloginfo('name'); echo ' - '; bloginfo('description'); } 
+              else {
+                  bloginfo('name'); }
+              if ($paged>1) { 
+                 echo ' - page '. $paged; } 
+           ?>
+        </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
